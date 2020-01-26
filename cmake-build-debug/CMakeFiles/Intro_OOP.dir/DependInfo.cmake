150ae78eb8b15f630b6fbc2c6a649c23ba0c3ab7
narimantos <narimantos@gmail.com>
2020-01-26 19:58:03 +0100

    Revert "ignore "
    This reverts commit ad7e06d6d1c637fb81426bbc61ac62cb79cb94db.

---
+++ cmake-build-debug/CMakeFiles/Intro_OOP.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/Develop/Git/OOP-HER/Proeftoets/DigitaleTicket.cpp" "E:/Develop/Git/OOP-HER/cmake-build-debug/CMakeFiles/Intro_OOP.dir/Proeftoets/DigitaleTicket.cpp.obj"
+  "E:/Develop/Git/OOP-HER/Proeftoets/PapierenTicket.cpp" "E:/Develop/Git/OOP-HER/cmake-build-debug/CMakeFiles/Intro_OOP.dir/Proeftoets/PapierenTicket.cpp.obj"
+  "E:/Develop/Git/OOP-HER/Proeftoets/Ticket.cpp" "E:/Develop/Git/OOP-HER/cmake-build-debug/CMakeFiles/Intro_OOP.dir/Proeftoets/Ticket.cpp.obj"
+  "E:/Develop/Git/OOP-HER/Proeftoets/TicketService.cpp" "E:/Develop/Git/OOP-HER/cmake-build-debug/CMakeFiles/Intro_OOP.dir/Proeftoets/TicketService.cpp.obj"
+  "E:/Develop/Git/OOP-HER/main.cpp" "E:/Develop/Git/OOP-HER/cmake-build-debug/CMakeFiles/Intro_OOP.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
